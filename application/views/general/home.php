83679e304b73075108a432d05f24cdbe10e6a42e
sreenathbs <sreenathbs97@gmail.com>
2018-08-23 19:16:56 +0530

    almost

--- application/views/general/home.php
+++ application/views/general/home.php
@@ -82,13 +82,14 @@
         <h2>together we begin !</h2>
         <h3>let's rebuild Kerala :)</h3>
         <div class="row">
-          <div class="span4">
+            <!--<div class="span3"></div>-->
+          <div class="span3">
             <a href="offer_service" class="btn-get-started scrollto">Offer Service</a>
           </div>
-          <div class="span4">
+<!--          <div class="hidden span4">
             <a href="request_service" class="btn-get-started scrollto">Request Service</a>
-          </div>
-          <div class="span4">
+          </div>-->
+          <div class="span3">
             <a href="nearby" class="btn-get-started scrollto">Nearby Services</a>
           </div>
         </div>
